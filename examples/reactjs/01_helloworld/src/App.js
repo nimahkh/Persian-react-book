f5dfa33b475d9fcc35913be343330fd0fb26d98d
nimahkh <nima.2004hkh@gmail.com>
2019-03-01 01:51:28 +0330

    Added 01_helloworld

---
+++ examples/reactjs/01_helloworld/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello World , Hello Iran .
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
